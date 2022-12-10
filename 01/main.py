5421669f111eedc24dafbe59dd630859f5e0333a
Rasmus Rynell <rynell.rasmus@gmail.com>
2022-12-10 21:27:21 +0100

    Even more clean

--- 01/main.py
+++ 01/main.py
@@ -2,10 +2,9 @@ import os
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as f:
-        data = f.read().split("\n\n") # Split into groups
-        data = [x.split("\n") for x in data] # Split into lines
-        data = [[int(y) for y in x] for x in data] # Convert to ints
-        data.sort(key=lambda x: sum(x), reverse=True) # Sort by sum
+        data = f.read().split("\n\n") # Split into individual elves 
+        data = [sum([int(y) for y in x.split("\n")]) for x in data] # Sum for each elf
+        data.sort(key=lambda x: x, reverse=True)
         
-        print(sum(data[0])) # Print sum of first group
-        print(sum(data[0]) + sum(data[1]) + sum(data[2])) # Print sum of first 3 groups
+        print(data[0])
+        print(sum(data[0:3]))
