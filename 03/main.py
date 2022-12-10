bd5617168999ab5f02825b1759435f5357126803
Rasmus Rynell <rynell.rasmus@gmail.com>
2022-12-10 22:21:22 +0100

    03 refactored

--- 03/main.py
+++ 03/main.py
@@ -8,32 +8,26 @@ def cnv(chr):
     return abs(ord(chr)) - 38
 
 def find_common_letters(strings):
-    def single(string1, string2):
-        return ''.join([x for x in string1 if x in string2])
-    return reduce(single, strings)[0]
+    return reduce(lambda string1, string2: ''.join([x for x in string1 if x in string2]), strings)[0]
 
 
 if __name__ == "__main__":
     total_sum_part1 = 0
-    with open('input.txt', 'r') as f:
-        for line in f:
-            line = line.replace('\n', '')
+    total_sum_part2 = 0
+    all_strings = []
+    
+    with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as f:
+        for idx, line in enumerate(f):
+            line = line.strip()
+            all_strings.append(line)
             first_half = line[:len(line)//2]
             second_half = line[len(line)//2:]
+
             total_sum_part1 += cnv(find_common_letters([first_half, second_half]))
+            
+            if (idx+1) % 3 == 0:
+                total_sum_part2 += cnv(find_common_letters(all_strings[-3:]))
 
     print(f"Part 1: {total_sum_part1}")
-
-
-    total_sum_part2 = 0
-    strings = []
-    with open('input.txt', 'r') as f:
-        for curr_iter, line in enumerate(f):
-            strings.append(line.replace('\n', ''))
-            
-            if (curr_iter+1) % 3 == 0:
-                total_sum_part2 += cnv(find_common_letters(strings))
-                strings = []
-
     print(f"Part 2: {total_sum_part2}")
 
