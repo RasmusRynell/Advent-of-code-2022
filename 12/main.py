42ceb4efc4044adb873c6aace0a1cde829c39ce0
Rasmus Rynell <rynell.rasmus@gmail.com>
2022-12-12 18:08:58 +0100

    12 part2 done

--- 12/main.py
+++ 12/main.py
@@ -6,18 +6,18 @@ import matplotlib.pyplot as plt
 
 full_map = []
 
-def search(full_map, start, end):
-    # Start is a tuple of (x, y)
-    # End is a tuple of (x, y)
-    # full_map is a 2D tensor of integers
-
+def search(full_map, start, end, reverse=False):
     width = full_map.shape[1]
     height = full_map.shape[0]
 
     def is_valid_neighbor(curr_node, next_node):
         if (next_node[0] >= 0 and next_node[0] < width) and (next_node[1] >= 0 and next_node[1] < height):
-            if full_map[next_node[1], next_node[0]] - full_map[curr_node[1], curr_node[0]] <= 1:
-                return True
+            if reverse:
+                if full_map[curr_node[1], curr_node[0]] - full_map[next_node[1], next_node[0]] <= 1:
+                    return True
+            else:
+                if full_map[next_node[1], next_node[0]] - full_map[curr_node[1], curr_node[0]] <= 1:
+                    return True
         return False
 
 
@@ -32,41 +32,39 @@ def search(full_map, start, end):
         if is_valid_neighbor(node, (node[0], node[1]+1)):
             neighbors.append((node[0], node[1]+1))
         return neighbors
-    
+
+    print(end)
+    print(start)
+
     # Breadth first search
     queue = [start]
     parent = {start: None}
     while queue:
         curr_node = queue.pop(0)
-        if curr_node == end:
-            print("Found the end node", flush=True)
-            break
         for neighbor in find_neighbors(curr_node):
-            print(f'\tparent[{curr_node}] added {curr_node}')
             if neighbor not in parent:
                 queue.append(neighbor)
                 parent[neighbor] = curr_node
+                if neighbor in end:
+                    real_end = neighbor
+                    # Break out of all loops
+                    queue = []
+                    break
+
     # Find the path
-    best_path = [end]
-    curr_node = end
+    best_path = [real_end]
+    curr_node = real_end
     while curr_node != start:
         curr_node = parent[curr_node]
         best_path.append(curr_node)
     best_path.reverse()
 
+    # Convert keys to strings and print
+    parent = {str(k): str(v) for k, v in parent.items()}
+    print(json.dumps(parent, indent=4))
+
     return best_path
 
-def visualize_map(full_map, parent, start, end):
-    # Plot the map
-    plt.imshow(full_map)
-
-    # Plot the path
-    curr_node = end
-    while curr_node != start:
-        curr_node = parent[curr_node]
-        plt.plot(curr_node[1], curr_node[0], 'r.')
-
-    plt.show()
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as f:
@@ -83,34 +81,36 @@ if __name__ == "__main__":
                 else:
                     full_map[y].append(ord(char)-97)
 
-    #end_node = (156,8)
-
     # make into tensor
     full_map = torch.tensor(full_map)
     print(f"Start node: {start_node}, value={full_map[start_node[1],start_node[0]]}", flush=True)
     print(f"End node: {end_node}, value={full_map[end_node[1],end_node[0]]}", flush=True)
 
-    # Search for the path
-    best_path = search(full_map, start_node, end_node)
-    #best_path = []
-    print(best_path, flush=True)
+    end_nodes = []
+    for y, row in enumerate(full_map):
+        for x, val in enumerate(row):
+            if val == 0:
+                end_nodes.append((x, y))
+                
+    best_paths = [
+        search(full_map, start_node, [end_node]),
+        search(full_map, end_node, end_nodes, reverse=True),
+    ]
 
     # Reverse (x, y) to (y, x)
-    best_path = [(x[1], x[0]) for x in best_path]
+    for path in best_paths:
+        for i, node in enumerate(path):
+            path[i] = (node[1], node[0])
 
     # Plot the map
     plt.imshow(full_map)
 
-    # Plot the path
-    x = [x[0] for x in best_path]
-    y = [x[1] for x in best_path]
-    plt.plot(y, x, 'r-')
+    # Plot the paths
+    for i in range(len(best_paths)):
+        x = [x[0] for x in best_paths[i]]
+        y = [x[1] for x in best_paths[i]]
+        plt.plot(y, x, 'r-' if i == 0 else 'b-')
     
-    print(f"Length {len(best_path)-1}", flush=True)
-
-    # Add text for start and end nodes
-    plt.text(start_node[0], start_node[1], 'S', color='r')
-    plt.text(end_node[0], end_node[1], 'E', color='r')
-
+        print(f"Part {i+1} len: {len(best_paths[i])-1}", flush=True)
 
     plt.show()
