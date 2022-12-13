a095614b3a3430c91ef32f1b2f51ee580fc9efd3
Rasmus Rynell <rynell.rasmus@gmail.com>
2022-12-13 14:16:29 +0100

    13 done

--- 13/main.py
+++ 13/main.py
@@ -4,64 +4,65 @@ import os
 
 
 def compare_packets(packet1, packet2):
-    if type(packet1) is not list or type(packet2) is not list:
-        print(f"Djud you got and error here: {packet1} vs {packet2}")
-        return False
+    if isinstance(packet1, int) and isinstance(packet2, int):
+        return -1 if packet1 < packet2 else 1 if packet2 < packet1 else 0 # -1 = ok, 0 = continue, 1 = not ok
+    elif isinstance(packet1, int) and isinstance(packet2, list):
+        return compare_packets([packet1], packet2)
+    elif isinstance(packet1, list) and isinstance(packet2, int):
+        return compare_packets(packet1, [packet2])
+    
+    if len(packet1) == 0 and len(packet2) != 0:
+        return -1
+    elif len(packet1) != 0 and len(packet2) == 0:
+        return 1
+    elif len(packet1) == 0 and len(packet2) == 0:
+        return 0
+    else:
+        result = compare_packets(packet1[0], packet2[0])
+        if result == 0:
+            return compare_packets(packet1[1:], packet2[1:])
+        return result
+    return 0
 
-    idx_1 = 0
-    idx_2 = 0
-    while idx_1 < len(packet1) and idx_2 < len(packet2):
-        print(f"\t- Compare {packet1[idx_1]} vs {packet2[idx_2]}", flush=True)
-        
-        if type(packet1[idx_1]) is int:
-            if type(packet2[idx_2]) is int:
-                if packet1[idx_1] < packet2[idx_2]:
-                    print("\t\t- Left side is smaller, so inputs are in the right order (int,int)", flush=True)
-                    return True
-                    
-                elif packet1[idx_1] > packet2[idx_2]:
-                    print("\t\t- Right side is smaller, so inputs are in the wrong order (int,int)", flush=True)
-                    return False
-        
-        if type(packet1[idx_1]) is list:
-            if type(packet2[idx_2]) is list:
-                if not compare_packets(packet1[idx_1], packet2[idx_2]):
-                    return False
-            else:
-                if not compare_packets(packet1[idx_1], [packet2[idx_2]]):
-                    return False
 
-        elif type(packet2[idx_2]) is list:
-            if not compare_packets([packet1[idx_1]], packet2[idx_2]):
-                return False
-
-        idx_1 += 1
-        idx_2 += 1
-
-    if idx_2 < len(packet2):
-        print("- Left side ran out of items, so inputs are in the right order")
-        return True
-    elif idx_1 < len(packet1):
-        print("- Right side ran out of items, so inputs are in the wrong order")
-        return False
-    return True
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(__file__), 'input_test.txt'), 'r') as f:
         data = f.read().split('\n\n')
+    with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as f:
+        data2 = f.read().split('\n\n')
 
-        correct = []
-        for pair_idx, pair_of_packets in enumerate(data):
-            print(f"== Pair {pair_idx+1} ==", flush=True)
-            (packet1, packet2) = pair_of_packets.split('\n')
-            packet1 = eval(packet1)
-            packet2 = eval(packet2)
+    
+    correct = []
+    correct2 = []
+    for pair_idx, pair_of_packets in enumerate(data):
+        (packet1, packet2) = pair_of_packets.split('\n')
+        if compare_packets(eval(packet1), eval(packet2)) == -1:
+            correct.append(pair_idx+1)
 
-            print(f"- Compare {packet1} vs {packet2}", flush=True)
-
-            if compare_packets(packet1, packet2):
-                correct.append(pair_idx+1)
-            
-            print()
+    for pair_idx, pair_of_packets in enumerate(data2):
+        (packet1, packet2) = pair_of_packets.split('\n')
+        if compare_packets(eval(packet1), eval(packet2)) == -1:
+            correct2.append(pair_idx+1)
 
     print(f"Correct: {correct}, sum: {sum(correct)}", flush=True)
+    print(f"Correct2: {correct2}, sum: {sum(correct2)}", flush=True)
+
+
+    temp = []
+    for pair_of_packets in data2:
+        (packet1, packet2) = pair_of_packets.split('\n')
+        temp.append(eval(packet1))
+        temp.append(eval(packet2))
+
+    temp.append([[2]])
+    temp.append([[6]])
+
+    # Sort using compare_packets
+    from functools import cmp_to_key
+    temp.sort(key=cmp_to_key(compare_packets))
+    
+    index_for_2 = temp.index([[2]])+1
+    index_for_6 = temp.index([[6]])+1
+
+    print(f"decoder key: {index_for_2*index_for_6}")
